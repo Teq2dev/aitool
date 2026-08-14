942c0937c7d494304b12a29dc7950098a5d9643f
evkisawari <137887334+evkisawari@users.noreply.github.com>
2026-08-14 21:48:05 +0530

    fix: include embeddings cache in Vercel bundle

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   experimental: {
     // Remove if not using Server Components
     serverComponentsExternalPackages: ['mongodb'],
+    outputFileTracingIncludes: {
+      '/*': ['./v6_field_embeddings_cache.json'],
+    },
   },
   webpack(config, { dev }) {
     if (dev) {
